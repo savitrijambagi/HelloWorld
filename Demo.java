9dacfcd735fddd10df02d468e900b192bf7f9685
savitrijambagi <savitrijambagi07@gmail.com>
2022-07-20 12:40:23 +0530

    Added today SE lab for B2 batch

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World today fun day");
+		System.out.println("Hello World today SE lab for B2 batch");
 	}
 }
