08f5703ffd194821e08a2c8fff5a9e321fbca2ab
savitrijambagi <savitrijambagi07@gmail.com>
2022-07-26 12:26:38 +0530

    Added today Hello world

--- Demo.java
+++ Demo.java
@@ -2,6 +2,6 @@ public class Demo
 {
 	public static void main(String args[])
 	{
-		System.out.println("Hello World today SE lab for B2 batch");
+		System.out.println("Added today Hello world");
 	}
 }
