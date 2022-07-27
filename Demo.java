77b6a5287906231d99167dc14f16b3065c3d1b78
savitrijambagi <savitrijambagi07@gmail.com>
2022-07-27 09:15:16 +0530

    Hello Software engineering

--- Demo.java
+++ Demo.java
@@ -1,7 +1,8 @@
-public class Demo
-{
-	public static void main(String args[])
-	{
-		System.out.println("Hello World today SOftware lab New");
-	}
-}
+class Simple
+{  
+    public static void main(String args[])
+{  
+     System.out.println("Hello Software engineering");  
+    }  
+} 
+} 
